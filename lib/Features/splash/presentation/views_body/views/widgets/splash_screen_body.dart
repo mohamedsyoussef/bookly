48f64f95da702f069c81400b959039338e28f6e0
mohamed_syoussef <mohamed.shaaban89@gmail.com>
2024-05-31 01:00:37 +0300

    Add animation to SplashScreenBody widget
    This commit adds an animation to the SplashScreenBody widget using the TickerProviderStateMixin and AnimationController. The animation slides the 'Read. Download. Share.' text into view.

--- lib/Features/splash/presentation/views_body/views/widgets/splash_screen_body.dart
+++ lib/Features/splash/presentation/views_body/views/widgets/splash_screen_body.dart
@@ -4,9 +4,39 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gap/gap.dart';
 
-class SplashScreenBody extends StatelessWidget {
+class SplashScreenBody extends StatefulWidget {
   const SplashScreenBody({super.key});
 
+  @override
+  State<SplashScreenBody> createState() => _SplashScreenBodyState();
+}
+
+class _SplashScreenBodyState extends State<SplashScreenBody> with TickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<Offset> slideAnimation;
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2), 
+  );
+  slideAnimation = Tween<Offset>(
+    begin: const Offset(0, 10),
+    end: Offset.zero,
+  ).animate(CurvedAnimation(
+    parent: _animationController,
+    curve: Curves.fastOutSlowIn,
+  ));
+  _animationController.forward();
+  
+  
+  }
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -23,10 +53,13 @@ class SplashScreenBody extends StatelessWidget {
               ),
             ),
             Gap(30.h),
-            Text(
-              'Read. Download. Share.',
-              style: TextStyle(fontSize: 20.sp),
-              textAlign: TextAlign.center,
+            SlideTransition(
+              position:slideAnimation ,
+              child: Text(
+                'Read. Download. Share.',
+                style: TextStyle(fontSize: 20.sp),
+                textAlign: TextAlign.center,
+              ),
             )
           ],
         ),
