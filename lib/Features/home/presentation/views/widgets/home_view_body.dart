4dbc43c1e26ded7937a3f81f490650815a5a163a
mohamed_syoussef <mohamed.shaaban89@gmail.com>
2024-06-02 15:37:43 +0300

    Refactor home view widgets
    - Extract BestSellerListView widget to a separate file
    - Create CustomRating widget to handle rating display
    - Update BestSellerListView to use CustomRating widget
    - Remove unused import statement in HomeViewBody widget

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,8 +1,8 @@
-import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gap/gap.dart';
+import 'best_seller_list_view.dart';
 import 'custom_app_bar.dart';
 import 'featured_book_list_view.dart';
 
@@ -36,66 +36,3 @@ class HomeViewBody extends StatelessWidget {
   }
 }
 
-class BestSellerListView extends StatelessWidget {
-  const BestSellerListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: 2800.w,
-      height: 105.h,
-      child: Row(
-        children: [
-          SizedBox(
-            width: 70.w,
-            height: 105.h,
-            child: Image.asset(AssetsData.testImage),
-          ),
-          Gap(30.w),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Book Name',
-                  style: Styles.gtSctraFineTextStyle20
-                      .copyWith(fontWeight: FontWeight.bold),
-                ),
-                Gap(2.h),
-                Text(
-                  'Book author',
-                  style: Styles.montesrratTextStyle18
-                      .copyWith(fontSize: 14.sp, fontWeight: FontWeight.w500),
-                ),
-                Gap(5.h),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      '19.99 \$',
-                      style: Styles.montesrratTextStyle18
-                          .copyWith(fontWeight: FontWeight.bold),
-                    ),
-                    Row(children: [
-                      const Icon(
-                        Icons.star_outlined,
-                        color: Colors.amber,
-                      ),
-                      Text(
-                        '4.5',
-                        style: Styles.montesrratTextStyle18
-                            .copyWith(fontSize: 16.sp),
-                      ),
-                      Gap(9.w),
-                      const Text('(2330)'),
-                    ])
-                  ],
-                )
-              ],
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
---
+++ lib/core/widgets/custom_rating.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:gap/gap.dart';
+
+import '../utils/styles.dart';
+
+class CustomRating extends StatelessWidget {
+  const CustomRating({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          '19.99 \$',
+          style: Styles.montesrratTextStyle18
+              .copyWith(fontWeight: FontWeight.bold, fontSize: 16.sp),
+        ),
+        Gap(20.w),
+        const Icon(
+          FontAwesomeIcons.solidStar,
+          color: Color(0xffFFDD4F),
+          size: 15,
+        ),
+        Gap(1.w),
+        Text(
+          '4.5',
+          style: Styles.montesrratTextStyle18.copyWith(fontSize: 16.sp),
+        ),
+        Gap(4.w),
+        Text(
+          '(2330)',
+          style: Styles.montesrratTextStyle18
+              .copyWith(fontSize: 14.sp, color: Colors.grey),
+        )
+      ],
+    );
+  }
+}
