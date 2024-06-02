44014040d66a58e5a3314cead4f62837e8563908
mohamed_syoussef <mohamed.shaaban89@gmail.com>
2024-06-02 23:41:43 +0300

    Refactor book details body widget and extract books action widget
    - Refactored the book details body widget by removing unused imports and extracting the books action widget into a separate file for better organization and reusability.
    - Removed the import of styles.dart from custom_button.dart as it was not being used.

--- lib/Features/home/presentation/views/widgets/book_details_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_body.dart
@@ -1,13 +1,10 @@
-import 'dart:ffi';
-
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/constants.dart';
 import 'package:bookly_app/core/utils/styles.dart';
-import 'package:bookly_app/core/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gap/gap.dart';
-
+import 'books_action.dart';
 import 'custom_book_details_app_bar.dart';
 import 'custom_book_details_rating.dart';
 
@@ -59,41 +56,3 @@ class BookDetailsBody extends StatelessWidget {
   }
 }
 
-class BooksAction extends StatelessWidget {
-  const BooksAction({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Expanded(
-          child: CustomButton(
-            textStyle: Styles.montesrratTextStyle18.copyWith(
-              fontWeight: FontWeight.bold,
-              fontSize: 16.sp,
-              color: Colors.black
-            ),
-            buttonLabel: '19.99 \$',
-            backgroundColor: Colors.white,
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(16),
-              bottomLeft: Radius.circular(16),
-            ),
-          ),
-        ),
-        Expanded(
-          child: CustomButton(
-            textStyle: Styles.gilroyTextStyle16.copyWith(color: Colors.white),
-            buttonLabel: 'Free Preview',
-            backgroundColor: const Color(0xffEF8262),
-            borderRadius: const BorderRadius.only(
-              topRight: Radius.circular(16),
-              bottomRight: Radius.circular(16),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-}
---
+++ lib/Features/home/presentation/views/widgets/books_action.dart
@@ -0,0 +1,43 @@
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/core/widgets/custom_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class BooksAction extends StatelessWidget {
+  const BooksAction({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Expanded(
+          child: CustomButton(
+            textStyle: Styles.montesrratTextStyle18.copyWith(
+              fontWeight: FontWeight.bold,
+              fontSize: 16.sp,
+              color: Colors.black
+            ),
+            buttonLabel: '19.99 \$',
+            backgroundColor: Colors.white,
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(16),
+              bottomLeft: Radius.circular(16),
+            ),
+          ),
+        ),
+        Expanded(
+          child: CustomButton(
+            textStyle: Styles.gilroyTextStyle16.copyWith(color: Colors.white),
+            buttonLabel: 'Free Preview',
+            backgroundColor: const Color(0xffEF8262),
+            borderRadius: const BorderRadius.only(
+              topRight: Radius.circular(16),
+              bottomRight: Radius.circular(16),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
