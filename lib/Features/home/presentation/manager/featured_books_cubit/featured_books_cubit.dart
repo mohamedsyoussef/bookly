8c4ff6ec632e4d26d904b156a8f2521591c736a7
mohamed_syoussef <mohamed.shaaban89@gmail.com>
2024-06-06 13:30:22 +0300

    Refactor FeaturedBooksCubit and update dependencies
    - Refactored FeaturedBooksCubit to use the HomeRepo for fetching featured books
    - Updated dependencies for bloc to version 8.1.4
    - Made bloc a direct dependency instead of a transitive one.

--- lib/Features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
+++ lib/Features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -1,10 +1,22 @@
-import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
-
+import '../../../../../core/errors/failure.dart';
 import '../../../data/models/book_model/book_model.dart';
-
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this._homeRepo) : super(FeaturedBooksInitial());
+  final HomeRepo _homeRepo;
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    Either<Failure, List<BookModel>> result =
+        await _homeRepo.fetchFeaturedBooks();
+    result.fold((failure) {
+      emit(FeaturedBooksFailure(failure.errMessage));
+    }, (books) {
+      emit(FeaturedBooksSuccess(books));
+    });
+  }
 }
