2e7e77b63b941bc19cf57ee9e2f85942207ae04c
mohamed_syoussef <mohamed.shaaban89@gmail.com>
2024-06-06 14:24:48 +0300

    Add dependency injection and BLoC providers to BooklyApp
    This commit adds dependency injection setup using service locator and BLoC providers for FeaturedBooksCubit and NewestBooksCubit to BooklyApp. This will allow for better state management and easier access to dependencies throughout the app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:bookly_app/Features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
+import 'package:bookly_app/Features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
 import 'package:bookly_app/core/utils/app_colors.dart';
+import 'package:bookly_app/core/utils/service_locator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'Features/home/data/repos/home_repo_implementation.dart';
 import 'core/routes/app_router.dart';
 
 void main() {
+  serviceLocatorSetUp();
   runApp(const BooklyApp());
 }
 
@@ -17,11 +23,25 @@ class BooklyApp extends StatelessWidget {
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (context, child) {
-        return MaterialApp.router(
-          theme:
-              ThemeData.dark().copyWith(scaffoldBackgroundColor: primaryColor),
-          routerConfig: AppRouter.router,
-          debugShowCheckedModeBanner: false,
+        return MultiBlocProvider(
+          providers: [
+            BlocProvider(
+              create: (context) => FeaturedBooksCubit(
+                getIt.get<HomeRepoImplement>(),
+              ),
+            ),
+            BlocProvider(
+              create: (context) => NewestBooksCubit(
+                getIt.get<HomeRepoImplement>(),
+              ),
+            )
+          ],
+          child: MaterialApp.router(
+            theme: ThemeData.dark()
+                .copyWith(scaffoldBackgroundColor: primaryColor),
+            routerConfig: AppRouter.router,
+            debugShowCheckedModeBanner: false,
+          ),
         );
       },
     );
