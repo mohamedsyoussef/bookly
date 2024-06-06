4df2054c04e25c2759331897e83d9a7f054655c7
mohamed_syoussef <mohamed.shaaban89@gmail.com>
2024-06-06 14:24:55 +0300

    Refactor service locator setup in service_locator.dart file

--- lib/core/utils/service_locator.dart
+++ lib/core/utils/service_locator.dart
@@ -3,13 +3,11 @@ import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
-class ServiceLocator {
-  final getIt = GetIt.instance;
+final getIt = GetIt.instance;
 
-  void setUp() {
-    getIt.registerSingleton<ApiService>(ApiService(Dio()));
-    getIt.registerSingleton<HomeRepoImplement>(
-      HomeRepoImplement(getIt.get<ApiService>()),
-    );
-  }
+void serviceLocatorSetUp() {
+  getIt.registerSingleton<ApiService>(ApiService(Dio()));
+  getIt.registerSingleton<HomeRepoImplement>(
+    HomeRepoImplement(getIt.get<ApiService>()),
+  );
 }
