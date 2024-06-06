c5162a3a982fcd22080965f81a53827610641d6f
mohamed_syoussef <mohamed.shaaban89@gmail.com>
2024-06-06 12:53:55 +0300

    Implement fetchFeaturedBooks method in HomeRepoImplement class
    This commit implements the fetchFeaturedBooks method in the HomeRepoImplement class. The method fetches a list of featured books from the Google Books API and returns them as a List of BookModel objects. If an error occurs during the API call, a ServerFailure is returned.

--- lib/Features/home/data/repos/home_repo_implementation.dart
+++ lib/Features/home/data/repos/home_repo_implementation.dart
@@ -34,8 +34,26 @@ class HomeRepoImplement implements HomeRepo {
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
+    try {
+      var response = await apiService.get('volumes?q=flutter');
+
+      List<BookModel> books = [];
+      for (var item in response['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(
+          ServerFailure.fromDioException(e),
+        );
+      }
+
+      return left(
+        ServerFailure(e.toString()),
+      );
+    }
   }
 }
